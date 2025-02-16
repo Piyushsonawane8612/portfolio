328d03c68e76f2b83ad2d029cadf8312a8cdcd52
Piyushsonawane8612 <79643311+Piyushsonawane8612@users.noreply.github.com>
2025-02-16 17:36:40 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -33,9 +33,6 @@ function Techstack() {
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
